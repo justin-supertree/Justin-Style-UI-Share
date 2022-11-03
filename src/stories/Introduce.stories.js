62e5b49eae9b380d76b279c7ed39ec8bed8de791
justin-supertree <justin1688@supertree.co>
2022-11-03 14:54:00 +0900

    feat: first setting

---
+++ src/stories/Introduce.stories.js
@@ -0,0 +1,10 @@
+import { storiesOf } from "@storybook/react";
+import React from "react";
+
+const stories = storiesOf("Introduce", module);
+
+stories.add("Introduce", () => {
+  return <div>Welcome to Justin's Styled Component</div>;
+});
+
+export default Introduce.stories;
